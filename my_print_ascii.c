9a8dbd4daea0e8394c9836a5fc21be413a8cf22b
LoickDab <167294503+LoickDab@users.noreply.github.com>
2024-04-17 10:25:42 +0000

    afficher la table ascii du caractère

---
+++ my_print_ascii.c
@@ -0,0 +1,26 @@
+void print_char(char c);
+
+
+void my_print_ascii(void){
+    
+        
+    int indice=32;
+    for (int i= 0; indice < i && indice <= 125;  i++){
+            print_char(i);
+        }
+
+    
+    
+    
+
+    }
+
+int main(void)
+{
+    my_print_ascii();
+    return 0;
+
+
+}
+
+
