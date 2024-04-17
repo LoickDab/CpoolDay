d2cc7793ba98fca02946eec9772cb007902626bb
LoickDab <167294503+LoickDab@users.noreply.github.com>
2024-04-17 11:23:07 +0000

    my_print_n_ascii

---
+++ my_print_n_ascii.c
@@ -0,0 +1,22 @@
+void print_char(char c);
+
+
+void my_print_n_ascii(int howMany){   
+    int i ;
+    for  (int i = 0; i <= howMany;  i++ ){
+        print_char(i);
+    }
+
+
+    
+    
+    
+
+    }
+
+int main(void)
+{
+my_print_n_ascii(5);
+return 0;
+}
+
